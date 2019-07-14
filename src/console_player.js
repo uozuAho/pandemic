58d27caaa553e3d4c26924e01516a360b34c02ff
Warwick Stone <uozu.aho@gmail.com>
2019-07-14 11:36:30 +1000

    start console loop, saga breaks on init

--- src/console_player.js
+++ src/console_player.js
@@ -1,5 +1,23 @@
 import configureStore from './store/configureStore.prod';
+import { moveInit } from './actions/mapActions';
+import readlineSync from 'readline-sync';
+import { createQuickGameInit } from './actions/globalActions';
 
 const store = configureStore();
 
-console.log(JSON.stringify(store.getState()));
+store.dispatch(createQuickGameInit(1));
+
+// console.log(JSON.stringify(store.getState()));
+
+// store.dispatch(moveInit(0));
+
+// console.log(JSON.stringify(store.getState()));
+
+readlineSync.promptCLLoop({
+    state: () => {
+        console.log(JSON.stringify(store.getState()));
+    },
+    q: () => { return true; }
+});
+
+console.log('Done');
