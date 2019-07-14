afa162dd066d940f9e556686bd87319653b7ec64
Warwick Stone <uozu.aho@gmail.com>
2019-07-14 12:06:50 +1000

    state to file, current move to console

--- src/console_player.js
+++ src/console_player.js
@@ -1,22 +1,30 @@
 import configureStore from './store/configureStore.prod';
 import { moveInit } from './actions/mapActions';
+import { createQuickGameInit, dealCardsInit } from './actions/globalActions';
+
 import readlineSync from 'readline-sync';
-import { createQuickGameInit } from './actions/globalActions';
+import fs from 'fs';
 
 const store = configureStore();
 
 store.dispatch(createQuickGameInit(1));
 
-// console.log(JSON.stringify(store.getState()));
-
-// store.dispatch(moveInit(0));
-
-// console.log(JSON.stringify(store.getState()));
+// todo: do i need to wait before dealing?
+store.dispatch(dealCardsInit());
 
 readlineSync.promptCLLoop({
-    state: () => {
-        console.log(JSON.stringify(store.getState()));
+    sc: () => {
+        const currentMoveState = JSON.stringify(store.getState().currentMove, null, 2);
+        console.log(currentMoveState);
     },
+    sf: () => {
+        // todo: cut out unnecessary stuff like map, state history
+        const stateStr = JSON.stringify(store.getState(), null, 2);
+        fs.writeFileSync('current_state.json', stateStr, e => {
+            console.log(e);
+        });
+    },
+    m: () => { store.dispatch(moveInit(0)); },
     q: () => { return true; }
 });
 
