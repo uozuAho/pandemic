4669aac1e978ca4b57e9e80fe6b28d35fb4b651a
Warwick Stone <uozu.aho@gmail.com>
2019-07-18 20:52:12 +1000

    WIP: abstracting command handler

--- src/console_game/console_player.js
+++ src/console_game/console_player.js
@@ -27,31 +27,62 @@ const getInterestingState = () => {
     }
 }
 
-readlineSync.promptCLLoop({
-    sc: () => {
-        const currentMoveState = JSON.stringify(store.getState().currentMove, null, 2);
-        console.log(currentMoveState);
-    },
-    // todo: command for full state, since it has map details
-    sf: () => {
-        const stateStr = JSON.stringify(getInterestingState(), null, 2);
-        fs.writeFileSync('current_state.json', stateStr, e => {
-            console.log(e);
-        });
-    },
-    init: () => {
-        store.dispatch(createQuickGameInit(1));
-        store.dispatch(dealCardsInit());
-        store.dispatch(animationDealCardsInitComplete());
-        store.dispatch(animationDealCardsComplete());
-        store.dispatch(animationInsertEpidemicCardsComplete());
-        while (store.getState().status === 'prepare') {
-            store.dispatch(animationDrawInfectionCardComplete());
+const commands = [
+    { name: 'help', description: 'print help info', handler: () => {console.log('yo')}},
+    { name: 'do', description: 'do stuff', handler: () => {console.log('do stuff')}}
+];
+
+class CommandHandler {
+
+    constructor(commands) {
+        this._commands = commands;
+    }
+
+    runLoop() {
+        const readlineSyncInput = this._mapCommandsToReadlineSyncInput(this._commands);
+        readlineSync.promptCLLoop(readlineSyncInput);
+    }
+
+    _mapCommandsToReadlineSyncInput(commands) {
+        const readlineSyncInput = {};
+        for (const command of commands) {
+            readlineSyncInput[command.name] = command.handler;
         }
-        console.log("now ready to play!");
-    },
-    m: () => { store.dispatch(moveInit(0)); },
-    q: () => { return true; }
-});
+        return readlineSyncInput;
+    }
+}
+
+const handler = new CommandHandler(commands);
+handler.runLoop();
+
+// readlineSync.promptCLLoop({
+//     help: () => {
+//         console.log('yo');
+//     },
+//     sc: () => {
+//         const currentMoveState = JSON.stringify(store.getState().currentMove, null, 2);
+//         console.log(currentMoveState);
+//     },
+//     // todo: command for full state, since it has map details
+//     sf: () => {
+//         const stateStr = JSON.stringify(getInterestingState(), null, 2);
+//         fs.writeFileSync('current_state.json', stateStr, e => {
+//             console.log(e);
+//         });
+//     },
+//     init: () => {
+//         store.dispatch(createQuickGameInit(1));
+//         store.dispatch(dealCardsInit());
+//         store.dispatch(animationDealCardsInitComplete());
+//         store.dispatch(animationDealCardsComplete());
+//         store.dispatch(animationInsertEpidemicCardsComplete());
+//         while (store.getState().status === 'prepare') {
+//             store.dispatch(animationDrawInfectionCardComplete());
+//         }
+//         console.log("now ready to play!");
+//     },
+//     m: () => { store.dispatch(moveInit(0)); },
+//     q: () => { return true; }
+// });
 
 console.log('Done');
